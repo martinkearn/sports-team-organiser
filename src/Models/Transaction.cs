ca0cfc98df71e2028bd17b723a158368b7049fe0
Martin Kearn <martin.kearn@microsoft.com>
2023-03-08 08:05:28 +0000

    Transactions

---
+++ src/Models/Transaction.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using Azure;
+using Azure.Data.Tables;
+
+namespace STO.Models
+{
+    public class Transaction : ITableEntity
+    {
+        [Required]
+        public string PlayerRowKey { get; set; } = default!;
+        [Required]
+        public double Amount { get; set; }
+        [Required]
+        public DateTimeOffset Date { get; set; } = DateTimeOffset.UtcNow!;
+        public string PartitionKey { get; set; } = default!;
+        public string RowKey { get; set; } = default!;
+        public DateTimeOffset? Timestamp { get; set; } = DateTimeOffset.UtcNow!;
+        public ETag ETag { get; set; } = default!;
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,7 @@
 global using STO.Services;
 global using STO.Interfaces;
 global using STO.Models;
+global using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 

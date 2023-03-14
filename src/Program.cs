fd504bf07ea3f269e4c77be97b37aca46f819bd5
Martin Kearn <martin.kearn@microsoft.com>
2023-03-14 09:33:13 +0000

    Added user name

--- src/Program.cs
+++ src/Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<IStorageService, StorageService>();
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddOptions<StorageConfiguration>()
     .Configure<IConfiguration>((settings, configuration) =>
     {

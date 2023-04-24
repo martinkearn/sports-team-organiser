47bb3efb693416cb5802a4635d3d59466bfc5a92
Martin Kearn <martinkearn@live.co.uk>
2023-04-24 14:40:17 +0100

    Routes to latest game

--- src/Controllers/RoutingController.cs
+++ src/Controllers/RoutingController.cs
@@ -3,8 +3,16 @@ using Microsoft.AspNetCore.Mvc;
 [Route("controllers/[controller]/[action]")]
 public class RoutingController : Controller
 {
-    public IActionResult LatestGame(int id)
+    IGameService _gameService;
+    public RoutingController(IGameService gameService)
     {
-        return Redirect("https://bing.com");
+        _gameService = gameService;
+    }
+
+    public async Task<IActionResult> LatestGame(int id)
+    {
+        var games = await _gameService.GetGames();
+        var latestGame = games.OrderByDescending(o => o.GameEntity.Date).FirstOrDefault();
+        return Redirect($"/games/{latestGame.GameEntity.RowKey}");
     }
 }

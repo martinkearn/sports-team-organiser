fe638a746ef1858a0d3e92cf8fb4590eda22fbfd
Martin Kearn <martinkearn@live.co.uk>
2024-01-29 14:03:09 +0000

    Auth working

--- src/Program.cs
+++ src/Program.cs
@@ -9,22 +9,36 @@ using Microsoft.Identity.Web.UI;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Rewrite;
 
+//Provided by template
 var builder = WebApplication.CreateBuilder(args);
 
-// Add dotnet services to the container.
+//Reads the app settings for AzureAdB2C and registers an authentication service
 builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAdB2C"));
+
+//AddMicrosoftIdentityUI adds the razor pages used for account management, login etc
 builder.Services.AddControllersWithViews()
     .AddMicrosoftIdentityUI();
+
+//Adds CascadingAuthenticationState to service collection to avoid having it in razor.
+//CascadingAuthenticationState exposes authentication data to razor pages and enables things like AuthorizeView
+builder.Services.AddCascadingAuthenticationState();
+
+//Required for Microsoft.Identity.Web.UI
+//Based on https://github.com/dotnet/aspnetcore/issues/52245
+builder.Services.AddRazorPages();
+
+//Provided by template
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+// Add custom auth policy
 builder.Services.AddAuthorization(config =>
     {
         config.AddPolicy("IsAdminEmail", policy => policy.Requirements.Add(new IsAdminEmailRequirement("martinkearn@live.co.uk")));
     });
 builder.Services.AddSingleton<IAuthorizationHandler, IsAdminEmailHandler>();
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents()
-    .AddMicrosoftIdentityConsentHandler();
-builder.Services.AddCascadingAuthenticationState();
 
 // Add custom services to the container
 builder.Services.AddSingleton<IStorageService, StorageService>();
@@ -38,9 +52,11 @@ builder.Services.AddOptions<StorageConfiguration>()
         configuration.GetSection(nameof(StorageConfiguration)).Bind(settings);
     });
 
+//Provided by template
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+//Provided by template
+//Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
@@ -48,23 +64,19 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+//Add Antiforgery Middleware to the request processing pipeline after the call to app.UseRouting. If there are calls to app.UseRouting and app.UseEndpoints, the call to app.UseAntiforgery must go between them. A call to app.UseAntiforgery must be placed after calls to app.UseAuthentication and app.UseAuthorization.
+//Based on https://learn.microsoft.com/en-us/aspnet/core/migration/70-80?view=aspnetcore-8.0&tabs=visual-studio
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-app.UseRouting();
 app.UseAuthorization();
 app.UseAntiforgery();
+
+//Needed for authentication provider routing
 app.MapControllers();
 
-// Redirect default built-in signed out page to root
-app.UseRewriter(new RewriteOptions().Add(
-    context =>
-    {
-        if (context.HttpContext.Request.Path.StartsWithSegments("/MicrosoftIdentity/Account/SignOut"))
-        {
-            context.HttpContext.Response.Redirect("/");
-        }
-    }));
+//Provided by template
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
+//Provided by template
 app.Run();

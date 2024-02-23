402988b974b812b41f031ccf19343a6e6e1aaf9a
Martin Kearn <martinkearn@live.co.uk>
2024-02-23 15:46:57 +0000

    Reverse Wasm project

--- src/STO.Client/Program.cs
+++ src/STO.Client/Program.cs
@@ -1,13 +1,5 @@
-global using STO.Models;
-global using STO.Models.Interfaces;
-global using STO.Services;
-
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-builder.Services.AddSingleton<IStorageService, StorageService>();
-builder.Services.AddSingleton<IGameService, GameService>();
-builder.Services.AddSingleton<IPlayerService, PlayerService>();
 
 await builder.Build().RunAsync();

c774cc9b84a66c490cb99b09136b03ad08cf5547
Martin Kearn <martinkearn@live.co.uk>
2024-02-27 14:19:47 +0000

    Create PlayerAtGameEntityController.cs

---
+++ src/STO.Api/Controllers/PlayerAtGameEntityController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using STO.Models;
+using STO.Models.Interfaces;
+using STO.Services;
+
+namespace STO.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PlayerAtGameEntityController : ControllerBase
+{
+    private readonly ILogger<PlayerAtGameEntityController> _logger;
+
+    private readonly IStorageService _storageService;
+
+    public PlayerAtGameEntityController(ILogger<PlayerAtGameEntityController> logger, IStorageService storageService)
+    {
+        _logger = logger;
+        _storageService = storageService;
+    }
+
+    [HttpGet(Name = "GetPlayerAtGameEntitys")]
+    public async Task<IEnumerable<PlayerAtGameEntity>> Get()
+    {
+        var playerEntitiesResult = await _storageService.QueryEntities<PlayerAtGameEntity>();
+        var playerEntities = playerEntitiesResult
+            .OrderBy(p => p.Name)
+            .ToList();
+
+        return playerEntities;
+    }
+
+    [HttpDelete(Name = "DeletePlayerAtGameEntity")]
+    public async Task Delete(string rowkey)
+    {
+        await _storageService.DeleteEntity<PlayerAtGameEntity>(rowkey);
+    }
+
+    [HttpPost(Name = "UpsertPlayerAtGameEntity")]
+    public async Task Post(PlayerAtGameEntity PlayerAtGameEntity)
+    {
+        await _storageService.UpsertEntity<PlayerAtGameEntity>(PlayerAtGameEntity);
+    }
+}

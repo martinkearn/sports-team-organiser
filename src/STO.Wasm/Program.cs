97b34022ad1b227eb6191824e06bc449b4d7965c
Martin Kearn <martin.kearn@microsoft.com>
2024-03-09 08:31:04 +0000

    Wasm Azure B2C auth (#199)
    * Try to get auth working
    
    * Auth working locally on new app registration
    
    * Update all settings files for new AAD
    
    * Different client id for staging
    
    * Exposing more environmental data on home page
    
    * Added prod app registration
    
    * Added <TrimmerRootAssembly Include="Microsoft.Authentication.WebAssembly.Msal" />
    
    * Added TrimmerRootAssembly
    
    * Expose claims data
    
    * Simpler IsAdmin check

--- src/STO.Wasm/Program.cs
+++ src/STO.Wasm/Program.cs
@@ -1,6 +1,10 @@
 global using STO.Models;
 global using STO.Models.Interfaces;
 global using STO.Services;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -17,21 +21,53 @@ public class Program
         // Httpclient
         builder.Services.AddHttpClient();
 
+        // Refer to https://learn.microsoft.com/en-us/aspnet/core/blazor/security/webassembly/standalone-with-azure-active-directory-b2c?view=aspnetcore-8.0
         builder.Services.AddMsalAuthentication(options =>
         {
             builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
+            options.ProviderOptions.DefaultAccessTokenScopes.Add("openid");
+            options.ProviderOptions.DefaultAccessTokenScopes.Add("offline_access");
+            options.ProviderOptions.LoginMode = "redirect";
         });
 
+        // Add custom auth policy
+        _ = builder.Services.AddAuthorizationCore(config =>
+            {
+                config.AddPolicy("IsAdmin", policy =>
+                    policy.RequireAssertion(context =>
+                    {
+                        if (!context.User.Identity.IsAuthenticated)
+                        {
+                            return false;
+                        }
+
+                        var email = context?.User.FindFirst(c => c.Type == ClaimTypes.Email)?.Value;
+                        var authName = context?.User?.Identity?.Name;
+                        if (authName == "Martin Kearn")
+                        {
+                            return true;
+                        }
+
+                        return false;
+                    })
+                );
+            });
+
+        // Add custom app settings
         builder.Services.AddOptions<StorageConfiguration>()
             .Configure<IConfiguration>((settings, configuration) =>
             {
                 configuration.GetSection(nameof(StorageConfiguration)).Bind(settings);
             });
+
+        // Add custom services
         builder.Services.AddSingleton<IStorageService, ApiStorageService>();
         builder.Services.AddSingleton<IPlayerService, PlayerService>();
         builder.Services.AddSingleton<IGameService, GameService>();
         builder.Services.AddSingleton<ITransactionService, TransactionService>();
         builder.Services.AddSingleton<IRatingService, RatingService>();
+
+        builder.Services.AddCascadingAuthenticationState();
 
         Console.WriteLine($"Client Hosting Environment: {builder.HostEnvironment.Environment}");
 

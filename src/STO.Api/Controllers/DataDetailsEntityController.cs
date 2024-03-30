bc510fd6a67040c083fe8e997e0b3803001d173d
Martin Kearn <martin.kearn@microsoft.com>
2024-03-30 20:08:07 +0000

    Added DataDetailsEntity post

--- src/STO.Api/Controllers/DataDetailsEntityController.cs
+++ src/STO.Api/Controllers/DataDetailsEntityController.cs
@@ -1,25 +1,30 @@
+using Azure.Data.Tables;
 using Microsoft.AspNetCore.Mvc;
 
 namespace STO.Api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class DataDetailsEntityController : ControllerBase
+public class DataDetailsEntityController(ILogger<GameEntityController> logger, IStorageService storageService) : ControllerBase
 {
-    private readonly ILogger<GameEntityController> _logger;
+    private readonly ILogger<GameEntityController> _logger = logger;
 
-    private readonly IStorageService _storageService;
+    private readonly IStorageService _storageService = storageService;
 
-    public DataDetailsEntityController(ILogger<GameEntityController> logger, IStorageService storageService)
-    {
-        _logger = logger;
-        _storageService = storageService;
-    }
-
-    [HttpGet(Name = "GetDataDetailsEntity")]
+	[HttpGet(Name = "GetDataDetailsEntity")]
     public async Task<IEnumerable<DataDetailsEntity>> Get()
     {
         var dataDetailsEntity = await _storageService.QueryEntities<DataDetailsEntity>();
         return dataDetailsEntity;
     }
+
+	[HttpPost(Name = "PostDataDetailsEntity")]
+	public async Task Post()
+	{
+		// Create DataDetailsEntity
+		var dataDetailsEntity = new DataDetailsEntity();
+
+		// Upsert entity
+		await _storageService.UpsertEntity<DataDetailsEntity>(dataDetailsEntity);
+	}
 }

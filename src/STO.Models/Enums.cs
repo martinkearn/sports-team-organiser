e828f36d1a10cef059401853e6385f574af75160
Martin Kearn <martinkearn@live.co.uk>
2024-09-24 09:50:44 +0100

    Updated team switching

--- src/STO.Models/Enums.cs
+++ src/STO.Models/Enums.cs
@@ -3,7 +3,7 @@ namespace STO.Models
     public abstract class Enums {
         public enum SortPagsBy { Name, Position, Team, GameBalance, PlayerBalance, Playing, Played };
         public enum SortTransactionsBy { Date, Amount, Player, Game };
-        public enum Team { A, B };
+        public enum Team { None, A, B };
         public enum PlayerPosition { Goalie,Defender,PlayMaker,BoxToBox,Midfielder,Forward }
         public enum SortRatingsBy { Rating, Player, Game };
 

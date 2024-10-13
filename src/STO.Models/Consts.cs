7c409c967982b2f52523ba4f48ca0646bc596eb1
Martin Kearn <martinkearn@live.co.uk>
2024-10-13 16:04:27 +0100

    Using consts for date format

---
+++ src/STO.Models/Consts.cs
@@ -0,0 +1,8 @@
+namespace STO.Models;
+
+public static class Consts
+{
+    public const string DateTimeFormat = "dd MMM yy HH:mm";
+    public const string JustTimeFormat = "HH:mm";
+    public const string JustDateFormat = "dd MMM yy";
+}
--- src/STO.Wasm/GlobalUsings.cs
+++ src/STO.Wasm/GlobalUsings.cs
@@ -2,10 +2,10 @@
 
 global using System;
 global using STO.Models;
+global using STO.Models.Interfaces;
+global using STO.Wasm;
 global using STO.Wasm.Services;
 global using STO.Wasm.Models;
 global using STO.Wasm.Interfaces;
-global using Blazored.LocalStorage;
-global using STO.Wasm;
 global using STO.Services;
-global using STO.Models.Interfaces;
+global using Blazored.LocalStorage;

5c6b011ecb7e51e2b0ef351c981a58ec488f4106
Martin Kearn <martinkearn@live.co.uk>
2025-02-16 08:25:59 +0000

    Create PlayerAtGameService with placeholder methods

---
+++ src/STO.Services/PlayerAtGameService.cs
@@ -0,0 +1,52 @@
+using STO.Models;
+using STO.Models.Interfaces;
+
+namespace STO.Services;
+
+public class PlayerAtGameService : IPlayerAtGameService
+{
+    public List<PlayerAtGame> GetPags(int? skip, int? take)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<PlayerAtGame> GetPagsForGame(string gameId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<PlayerAtGame> OrganiseTeams(List<PlayerAtGame> pags)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task ResetTeamsAsync(List<PlayerAtGame> pags)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task TogglePagPlayedAsync(string pagId, bool played)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Game GetPag(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Game GetPagByUrlSegment(string urlSegment)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task DeletePagAsync(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task UpsertPagAsync(PlayerAtGame pag)
+    {
+        throw new NotImplementedException();
+    }
+}
